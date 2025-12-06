814dfa2eeb28a4c653ab339926f6e48527dc6e28
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-12-06 13:57:05 +0500

    Update build script to use supervisord
    Replaced direct execution of Django with supervisord.

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-python telegram_bot.py &
+# Запуск через supervisor
 
-# Запускаем Django (на переднем плане)
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+exec supervisord -c supervisord.conf
