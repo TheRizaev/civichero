ee776005f6559cf61af7a568ee8d0ef25d09cf5f
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-12-06 16:08:02 +0500

    Update start.sh
    Remove background execution of the Telegram bot.

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 # Запуск Django и Telegram бота вместе
 
-# Запускаем бота в фоне
 python telegram_bot.py &
 
-# Запускаем Django (на переднем плане)
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
