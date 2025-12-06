45af2049465071939aff0e27e15fcc0516405a66
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-12-06 13:40:53 +0500

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,4 +1,8 @@
-# Конфигурация бота
-# Замените на ваш токен от @BotFather
+import os
 
-BOT_TOKEN = "8455100116:AAG5Joe1slEAQGRNPjZ845Apb_M15P5AanE"
+# Конфигурация бота
+# В продакшене токен берется из переменной окружения
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8455100116:AAG5Joe1slEAQGRNPjZ845Apb_M15P5AanE')
+
+# Порт HTTP сервера (на Render может быть переопределен)
+WEBHOOK_PORT = int(os.environ.get('PORT', 8001))
