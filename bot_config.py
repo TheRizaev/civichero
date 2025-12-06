b22cc60222e36a406056549488ffec22713ab2f8
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-12-06 13:54:01 +0500

    Change WEBHOOK_PORT to a fixed value of 8001
    Set the HTTP server port to a fixed value.

--- bot_config.py
+++ bot_config.py
@@ -4,5 +4,4 @@ import os
 # В продакшене токен берется из переменной окружения
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '8455100116:AAG5Joe1slEAQGRNPjZ845Apb_M15P5AanE')
 
-# Порт HTTP сервера (на Render может быть переопределен)
-WEBHOOK_PORT = int(os.environ.get('PORT', 8001))
+WEBHOOK_PORT = 8001
