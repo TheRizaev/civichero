9ae5560577b688010d577dc25a481436c1c6fffa
Амир <158758261+TheRizaev@users.noreply.github.com>
2025-12-06 16:03:21 +0500

    Update bot_config.py
    Removed comments regarding bot configuration and token retrieval.

--- bot_config.py
+++ bot_config.py
@@ -1,7 +1,5 @@
 import os
 
-# Конфигурация бота
-# В продакшене токен берется из переменной окружения
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '8455100116:AAG5Joe1slEAQGRNPjZ845Apb_M15P5AanE')
 
 WEBHOOK_PORT = 8001
